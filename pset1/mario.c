e05ade1994c4f8b73be0626aa7d90fbc4b927ea3
xysey <xysey6@gmail.com>
2017-03-15 11:07:44 +0800

    updated mario.c

--- pset1/mario.c
+++ pset1/mario.c
@@ -6,15 +6,48 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void drawPyramid(int height);
+
 int main(void)
 {
-    int pyramidHeight = 0;
-   //check the value of the int
-   do
-   {
+    int height = 0;
+    //check the height
+    do
+    {
         printf("height: ");
-        pyramidHeight = GetInt();
-   }while(pyramidHeight <= 0 || pyramidHeight >= 24 )
-   
+        height = GetInt();
+    }while(height < 0 || height > 23);
+    
+    //draw pyramid
+    drawPyramid(height);
     return 0;
+}
+
+void drawPyramid(int height)
+{
+    for (int superCounter = height; superCounter > 0; superCounter--)
+    {
+        //print left pyramid
+        for (int leftCounter = 0; leftCounter < height; leftCounter++)
+        {
+            //print spaces as long as leftCounter is less than the value of superCounter - 1
+            if (leftCounter < superCounter - 1)
+            {
+                printf(" ");
+            }
+            else //print hashes
+            {
+                printf("#");
+            }
+        }
+         
+        //pring the gap
+        printf("  ");
+        //print right pyramid
+        for (int rightCounter = 0; rightCounter <= height - superCounter; rightCounter++)
+        {
+           printf("#");  
+        }
+        printf("\n");
+    }
 }
